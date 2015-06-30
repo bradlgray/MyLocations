bc54514eccfbbd93c1b1bd77a31e27ed3ca384f4
Bradford Gray <bgray@westmont.edu>
2015-06-30 14:43:34 -0700

    address fits and scales and fixed a bug

--- LocationDetailsViewController.swift
+++ LocationDetailsViewController.swift
@@ -7,6 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
+
+private let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.dateStyle = .MediumStyle
+    formatter.timeStyle = .ShortStyle
+    return formatter
+}()
 
 class LocationDetailsViewController: UITableViewController {
     
@@ -18,6 +26,13 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
    
+   
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
+    var descriptionText = ""
+    
+
     
     @IBAction func done() {
         dismissViewControllerAnimated(true, completion: nil)
@@ -27,10 +42,11 @@ class LocationDetailsViewController: UITableViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        descriptionTextView.text = ""
+        descriptionTextView.text = descriptionText
         categoryLabel.text = ""
         
         latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
@@ -48,10 +64,26 @@ class LocationDetailsViewController: UITableViewController {
     func stringFromPlacemark(placemark: CLPlacemark) -> String {
         return
             "\(placemark.subThoroughfare) \(placemark.thoroughfare), " + "\(placemark.locality), " +
-                "\(placemark.administrativeArea) \(placemark.postalCode)," + "\(placemark.country)"    }
+                "\(placemark.administrativeArea) \(placemark.postalCode)," + "\(placemark.country)"
+    }
     
+    func formatDate(date: NSDate) -> String {
+        return dateFormatter.stringFromDate(date)
+    }
     
-    
-    
-    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            addressLabel.frame.size = CGSize(
+                width: view.bounds.size.width - 115, height: 10000)
+            
+            
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 15
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+    }
 }
